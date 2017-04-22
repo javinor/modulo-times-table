056581e2d5d966a65c5e3b39b4d6c6527c57990c
javinor <javinor@gmail.com>
2017-04-22 23:05:37 +0300

    remove defaults

--- src/ModuloTimesTable.js
+++ src/ModuloTimesTable.js
@@ -1,7 +1,7 @@
 'use strict'
 
 export default class ModuloTimeTable {
-  constructor (canvas, modulo = 60, multiplier = 2) {
+  constructor (canvas, modulo, multiplier) {
     this.canvas = canvas
     this._modulo = modulo
     this._multiplier = multiplier
@@ -40,15 +40,13 @@ export default class ModuloTimeTable {
     const ctx = this.canvas.getContext('2d')
     const height = this.canvas.height
     const width = this.canvas.width
-    // const centerX = width / 2
-    // const centerY = height / 2
 
     ctx.clearRect(0, 0, width, height)
     ctx.beginPath()
 
     for (let i = 0; i < this.modulo; i++) {
-      let from = this.getNthPoint(i)
-      let to = this.getNthPoint(i * this.multiplier % this.modulo)
+      const from = this.getNthPoint(i)
+      const to = this.getNthPoint(i * this.multiplier % this.modulo)
 
       ctx.moveTo(from.x, from.y)
       ctx.lineTo(to.x, to.y)
